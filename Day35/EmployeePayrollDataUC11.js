fec18380ea8f3777ff4a7d271ad291716d390fda
unknown <jimpidas>
2021-06-22 17:56:13 +0530

    [Jyosmita]Added:Employee payroll data

---
+++ Day35/EmployeePayrollDataUC11.js
@@ -0,0 +1,28 @@
+//UC11 JS Classes
+class EmployeePayrollData{
+    id;
+    salary;
+    
+        constructor(id,name,salary)
+        {
+            this.id=id;
+            this.name=name;
+            this.salary=salary;
+        }
+    
+        get name(){return this._name; }
+        set name(name){this._name=name; }
+        get salary(){return this._salary;}
+        set salary(salary){this._salary=salary; }
+    
+        toString(){
+            return "id="+this.id +",name="+this.name+",salary="+this.salary;
+        }
+    }
+    let employeePayrollData=new EmployeePayrollData(1,"Mark",30000);
+    console.log(employeePayrollData.toString());
+    employeePayrollData.name="John";
+    employeePayrollData.salary=20000;
+    console.log(employeePayrollData.toString());
+    
+
